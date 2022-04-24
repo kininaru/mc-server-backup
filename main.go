7742d2b082761ba3b28e128e983b01e71948dbce
kininaru <shiftregister233@outlook.com>
2022-04-24 10:41:58 +0800

    finish zip

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	zip_tools "github.com/kininaru/mc-server-backup/zip-tools"
+	"os"
+	"strconv"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+	if _, err := os.Stat("./mc-backup"); os.IsNotExist(err) {
+		_ = os.Mkdir("./mc-backup", 0777)
+	}
+	zip_tools.ToZip("./test", "./mc-backup/backup"+strconv.Itoa(int(time.Now().UnixNano()))+".zip")
+	fmt.Println("Zipping finished. Uploading...")
 }

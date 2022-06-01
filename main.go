d2eaf292f733ab2b098e0c028a8b90d3859aeda6
kininaru <shiftregister233@outlook.com>
2022-06-01 18:58:51 +0800

    changed upload logic

--- main.go
+++ main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/kininaru/mc-server-backup/upload"
-	zip_tools "github.com/kininaru/mc-server-backup/zip-tools"
 	"log"
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/kininaru/mc-server-backup/upload"
+	zip_tools "github.com/kininaru/mc-server-backup/zip-tools"
 )
 
 func main() {
-	if len(os.Args) != 5 {
-		log.Fatalln("Arguments not enough")
-	}
 	if _, err := os.Stat("./mc-backup"); os.IsNotExist(err) {
 		_ = os.Mkdir("./mc-backup", 0777)
 	}
 	filename := "backup" + strconv.Itoa(int(time.Now().UnixNano())) + ".zip"
 	zip_tools.ToZip("./mc", "./mc-backup/"+filename)
+	if len(os.Args) != 5 {
+		log.Fatalln("Arguments not enough. Won't upload to OSS.")
+	}
 	fmt.Println("Zipping finished. Uploading...")
 	upload.ToAliyun(os.Args[1], os.Args[2], os.Args[3], os.Args[4], filename)
 	fmt.Println("Finished. Filename is " + filename)
